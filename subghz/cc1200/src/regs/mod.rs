b4d8b8e9a49c11a38deb1cdf7a3379a9e4609e9f
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-12-23 15:22:32 +0100

    Generate register values as enum where possible

---
+++ subghz/cc1200/src/regs/mod.rs
@@ -0,0 +1,142 @@
+use core::mem::transmute;
+
+use crate::gpio::{Gpio0Output, Gpio1Output, Gpio2Output, Gpio3Output, GpioOutput};
+
+mod generated;
+pub use generated::*;
+
+pub trait Iocfg {
+    fn gpio_cfg(&self) -> Option<GpioOutput>;
+    fn set_gpio_cfg(&mut self, value: GpioOutput);
+}
+
+impl Iocfg3 {
+    pub fn gpio3_cfg_value(&self) -> Gpio3Output {
+        unsafe { transmute(self.gpio3_cfg()) }
+    }
+}
+
+impl Iocfg for Iocfg3 {
+    fn gpio_cfg(&self) -> Option<GpioOutput> {
+        self.gpio3_cfg().try_into().ok()
+    }
+
+    fn set_gpio_cfg(&mut self, value: GpioOutput) {
+        self.set_gpio3_cfg(value as u8);
+    }
+}
+
+impl Iocfg2 {
+    pub fn gpio2_cfg_value(&self) -> Gpio2Output {
+        unsafe { transmute(self.gpio2_cfg()) }
+    }
+}
+
+impl Iocfg for Iocfg2 {
+    fn gpio_cfg(&self) -> Option<GpioOutput> {
+        self.gpio2_cfg().try_into().ok()
+    }
+
+    fn set_gpio_cfg(&mut self, value: GpioOutput) {
+        self.set_gpio2_cfg(value as u8);
+    }
+}
+
+impl Iocfg1 {
+    pub fn gpio1_cfg_value(&self) -> Gpio1Output {
+        unsafe { transmute(self.gpio1_cfg()) }
+    }
+}
+
+impl Iocfg for Iocfg1 {
+    fn gpio_cfg(&self) -> Option<GpioOutput> {
+        self.gpio1_cfg().try_into().ok()
+    }
+
+    fn set_gpio_cfg(&mut self, value: GpioOutput) {
+        self.set_gpio1_cfg(value as u8);
+    }
+}
+
+impl Iocfg0 {
+    pub fn gpio0_cfg_value(&self) -> Gpio0Output {
+        unsafe { transmute(self.gpio0_cfg()) }
+    }
+}
+
+impl Iocfg for Iocfg0 {
+    fn gpio_cfg(&self) -> Option<GpioOutput> {
+        self.gpio0_cfg().try_into().ok()
+    }
+
+    fn set_gpio_cfg(&mut self, value: GpioOutput) {
+        self.set_gpio0_cfg(value as u8);
+    }
+}
+
+impl FifoCfg {
+    pub fn bytes_in_rxfifo(&self) -> u8 {
+        self.fifo_thr() + 1
+    }
+
+    pub fn set_bytes_in_rxfifo(&mut self, value: u8) {
+        assert!((1..=128).contains(&value));
+        self.set_fifo_thr(value - 1);
+    }
+
+    pub fn bytes_in_txfifo(&self) -> u8 {
+        127 - self.fifo_thr()
+    }
+
+    pub fn set_bytes_in_txfifo(&mut self, value: u8) {
+        assert!(value <= 127);
+        self.set_fifo_thr(127 - value);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fifo_thr_rx() {
+        let mut fifocfg = FifoCfg(0);
+        
+        fifocfg.set_bytes_in_rxfifo(1);
+        assert_eq!(1, fifocfg.bytes_in_rxfifo());
+        assert_eq!(0, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_rxfifo(2);
+        assert_eq!(2, fifocfg.bytes_in_rxfifo());
+        assert_eq!(1, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_rxfifo(127);
+        assert_eq!(127, fifocfg.bytes_in_rxfifo());
+        assert_eq!(126, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_rxfifo(128);
+        assert_eq!(128, fifocfg.bytes_in_rxfifo());
+        assert_eq!(127, fifocfg.fifo_thr());
+    }
+
+    #[test]
+    fn fifo_thr_tx() {
+        let mut fifocfg = FifoCfg(0);
+        
+        fifocfg.set_bytes_in_txfifo(127);
+        assert_eq!(127, fifocfg.bytes_in_txfifo());
+        assert_eq!(0, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_txfifo(126);
+        assert_eq!(126, fifocfg.bytes_in_txfifo());
+        assert_eq!(1, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_txfifo(1);
+        assert_eq!(1, fifocfg.bytes_in_txfifo());
+        assert_eq!(126, fifocfg.fifo_thr());
+
+        fifocfg.set_bytes_in_txfifo(0);
+        assert_eq!(0, fifocfg.bytes_in_txfifo());
+        assert_eq!(127, fifocfg.fifo_thr());
+    }
+}
