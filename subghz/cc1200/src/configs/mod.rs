9d70d233260c35cc09176fcdfc4d4248e1091b43
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-01-04 11:12:20 +0100

    Add LinkIQ config

--- subghz/cc1200/src/configs/mod.rs
+++ subghz/cc1200/src/configs/mod.rs
@@ -1,9 +1,11 @@
 mod diehlr4;
+mod linkiq;
 mod wmbus_modecmto;
 mod wmbus_modetmto;
 mod wmbus_modetmto_diehl;
 
 pub use diehlr4::*;
+pub use linkiq::*;
 pub use wmbus_modecmto::*;
 pub use wmbus_modetmto::*;
 pub use wmbus_modetmto_diehl::*;
