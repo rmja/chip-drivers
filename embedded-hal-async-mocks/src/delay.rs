9b576d5783544076018eb5e6cdc85e1efc96bfe3
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-05-07 15:47:34 +0200

    Align mocks with new embedded-hal version

--- embedded-hal-async-mocks/src/delay.rs
+++ embedded-hal-async-mocks/src/delay.rs
@@ -1,17 +1,12 @@
 use embedded_hal_async::delay;
 use mockall::mock;
 
-#[derive(Debug, Clone, Copy)]
-pub struct DelayError;
-
 mock! {
     #[derive(Debug)]
     pub Delay {}
 
     impl delay::DelayUs for Delay {
-        type Error = DelayError;
-
-        async fn delay_us(&mut self, us: u32) -> Result<(), DelayError>;
-        async fn delay_ms(&mut self, ms: u32) -> Result<(), DelayError>;
+        async fn delay_us(&mut self, us: u32);
+        async fn delay_ms(&mut self, ms: u32);
     }
 }
