7ddd684056388b0bbae52212c0ce1714d8ac6cda
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-06-03 11:57:21 +0200

    Extend timeout for sim900 close connection

--- cellular/simcom-gprs/src/commands/tcpip/impls/closeconnection.rs
+++ cellular/simcom-gprs/src/commands/tcpip/impls/closeconnection.rs
@@ -12,9 +12,10 @@ use crate::commands::{
 impl AtatCmd<35> for CloseConnection {
     type Response = CloseOk;
 
-    // There is no timeout documentation for sim900, but it is more than one second.
+    // There is no timeout documentation for sim900
+    // It has been observed that e.g. "0, CLOSE" arrives up to 10 seconds after "AT+CIPCLOSE=0"
     #[cfg(feature = "sim900")]
-    const MAX_TIMEOUT_MS: u32 = 2000;
+    const MAX_TIMEOUT_MS: u32 = 10_000;
 
     fn as_bytes(&self) -> heapless::Vec<u8, 35> {
         let inner = CloseConnectionInner { id: self.id };
