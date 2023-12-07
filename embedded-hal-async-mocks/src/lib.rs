6c10262d7427c295886991c87a8d81f490c0ab7b
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-12-07 09:53:58 +0100

    Add MockSpiDevice::expect_transaction_operations()

--- embedded-hal-async-mocks/src/lib.rs
+++ embedded-hal-async-mocks/src/lib.rs
@@ -1,2 +1,4 @@
+#![feature(let_chains)]
+
 pub mod delay;
 pub mod spi;
--- embedded-hal-async-mocks/src/spi.rs
+++ embedded-hal-async-mocks/src/spi.rs
@@ -1,4 +1,4 @@
-use embedded_hal_async::spi;
+use embedded_hal_async::spi::{self, Operation};
 use mockall::mock;
 
 #[derive(Debug, Clone, Copy)]
@@ -20,5 +20,59 @@ mock! {
 
     impl<Word: Copy + 'static> spi::ErrorType for SpiDevice<Word> {
         type Error = SpiError;
+    }
+}
+
+impl MockSpiDevice<u8> {
+    pub fn expect_transaction_operations(&mut self, expected: &'static [Operation<'static, u8>]) {
+        self.expect_transaction()
+            .withf(move |transaction| {
+                if transaction.len() != expected.len() {
+                    return false;
+                }
+                for (actual, expected) in transaction.iter().zip(expected) {
+                    if !Self::is_match(actual, expected) {
+                        return false;
+                    }
+                }
+
+                true
+            })
+            .returning(move |transaction| {
+                for (dest, src) in transaction.iter_mut().zip(expected) {
+                    Self::assign(dest, src);
+                }
+                Ok(())
+            });
+    }
+
+    fn is_match(x: &Operation<'_, u8>, y: &Operation<'_, u8>) -> bool {
+        if let Operation::Read(x) = x
+            && let Operation::Read(y) = y
+        {
+            x.len() == y.len()
+        } else if let Operation::Write(x) = x
+            && let Operation::Write(y) = y
+        {
+            x == y
+        } else if let Operation::Transfer(_, x) = x
+            && let Operation::Transfer(_, y) = y
+        {
+            x == y
+        } else {
+            false
+        }
+    }
+
+    fn assign(dest: &mut Operation<'_, u8>, src: &Operation<'_, u8>) {
+        if let Operation::Read(dest) = dest
+            && let Operation::Read(src) = src
+        {
+            dest.copy_from_slice(src)
+        } else if let Operation::Transfer(dest, _) = dest
+            && let Operation::Transfer(src, _) = src
+        {
+            dest.copy_from_slice(src)
+        }
     }
 }
